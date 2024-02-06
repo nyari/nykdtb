cdc3b826c0419e1678082ee5fa6a1a5385eb407b
Nyári-Kovács, Dávid Tamás <dev@dnyari.info>
2024-02-06 22:02:24 +0100

    Add elementwise operations

--- targets/nykdtb_lib/nykdtb/ndarray_ops.hpp
+++ targets/nykdtb_lib/nykdtb/ndarray_ops.hpp
@@ -9,9 +9,10 @@ namespace nykdtb::nda {
 
 NYKDTB_DEFINE_EXCEPTION_CLASS(ShapesDoNotMatch, LogicException);
 NYKDTB_DEFINE_EXCEPTION_CLASS(SizesDoNotMatch, LogicException);
+NYKDTB_DEFINE_EXCEPTION_CLASS(DivisionByZero, RuntimeException);
 
-template<NDArrayLike LHS, NDArrayLike RHS>
-inline static void addAssign(LHS& lhs, const RHS& rhs) {
+template<NDArrayLike LHS, NDArrayLike RHS, typename F>
+inline static void baseAssignWithSameShape(LHS& lhs, const RHS& rhs, F op) {
     if (lhs.shape() != rhs.shape()) {
         throw ShapesDoNotMatch();
     }
@@ -21,13 +22,51 @@ inline static void addAssign(LHS& lhs, const RHS& rhs) {
     auto rhsIt    = rhs.begin();
 
     for (auto lhsIt = lhsBegin; lhsIt < lhsEnd; ++lhsIt, ++rhsIt) {
-        *lhsIt += *rhsIt;
+        op(*lhsIt, *rhsIt);
     }
+}
+
+template<NDArrayLike LHS, NDArrayLike RHS>
+inline static void addAssign(LHS& lhs, const RHS& rhs) {
+    baseAssignWithSameShape(lhs, rhs, [](auto& lhs, const auto& rhs) { lhs += rhs; });
 }
 
 template<NDArrayLike LHS, NDArrayLike RHS>
 inline static LHS add(LHS lhs, const RHS& rhs) {
     addAssign(lhs, rhs);
+    return mmove(lhs);
+}
+
+template<NDArrayLike LHS, NDArrayLike RHS>
+inline static void subAssign(LHS& lhs, const RHS& rhs) {
+    baseAssignWithSameShape(lhs, rhs, [](auto& lhs, const auto& rhs) { lhs -= rhs; });
+}
+
+template<NDArrayLike LHS, NDArrayLike RHS>
+inline static LHS sub(LHS lhs, const RHS& rhs) {
+    subAssign(lhs, rhs);
+    return mmove(lhs);
+}
+
+template<NDArrayLike LHS, NDArrayLike RHS>
+inline static void ewMulAssign(LHS& lhs, const RHS& rhs) {
+    baseAssignWithSameShape(lhs, rhs, [](auto& lhs, const auto& rhs) { lhs *= rhs; });
+}
+
+template<NDArrayLike LHS, NDArrayLike RHS>
+inline static LHS ewMul(LHS lhs, const RHS& rhs) {
+    ewMulAssign(lhs, rhs);
+    return mmove(lhs);
+}
+
+template<NDArrayLike LHS, NDArrayLike RHS>
+inline static void ewDivAssign(LHS& lhs, const RHS& rhs) {
+    baseAssignWithSameShape(lhs, rhs, [](auto& lhs, const auto& rhs) { lhs /= rhs; });
+}
+
+template<NDArrayLike LHS, NDArrayLike RHS>
+inline static LHS ewDiv(LHS lhs, const RHS& rhs) {
+    ewDivAssign(lhs, rhs);
     return mmove(lhs);
 }
 
@@ -51,6 +90,9 @@ inline static void normalize(T& elem) {
     typename T::Type lengthsq = 0;
     for (const auto& e : elem) {
         lengthsq += e * e;
+    }
+    if (lengthsq == 0.0) {
+        throw DivisionByZero();
     }
     auto mtp = 1.0F / std::sqrt(lengthsq);
     for (auto& e : elem) {
